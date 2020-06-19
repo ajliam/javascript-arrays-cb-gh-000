5a6c57cf461e6df95a82ef2328193ea7a2607467
Aj Liam <andrewjordan35@gmail.com>
2020-06-19 21:15:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 
 
 var chocolateBars = ["snickers", "hundred gran", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array,element){
+  return [element, ...array]
+}
+
+
+function destructivelyAddElementToBeginningOfArray(,) 
